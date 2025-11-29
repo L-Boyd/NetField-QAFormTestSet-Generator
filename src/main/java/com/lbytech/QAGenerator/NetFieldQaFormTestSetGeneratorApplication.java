691430bff311b12f781d125b97873b874c4caece
lby-magicbook <1508201988@qq.com>
2025-11-29 17:24:27 +0800

    feat：“针对网络问题解答大模型生成QA测试集的大模型应用”项目初始化

---
+++ src/main/java/com/lbytech/QAGenerator/NetFieldQaFormTestSetGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.lbytech.QAGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetFieldQaFormTestSetGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetFieldQaFormTestSetGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lbytech/QAGenerator/NetFieldQaFormTestSetGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbytech.QAGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetFieldQaFormTestSetGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
