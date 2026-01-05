c66297e6bb34e18eaf31581904e0ca373a39e1fe
lby-magicbook <1508201988@qq.com>
2026-01-05 16:49:54 +0800

    feat：引入langchain4j依赖，并配置

--- src/main/java/com/lbytech/QAGenerator/NetFieldQaFormTestSetGeneratorApplication.java
+++ src/main/java/com/lbytech/QAGenerator/NetFieldQaFormTestSetGeneratorApplication.java
@@ -2,8 +2,10 @@ package com.lbytech.QAGenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:environmentVariables.env")
 public class NetFieldQaFormTestSetGeneratorApplication {
 
     public static void main(String[] args) {
